0fc280b74e5b5fa65ab4840e99b7aa1539076eed
William <145863364+WilliamMark1963@users.noreply.github.com>
2026-05-14 17:39:06 +0530

    At frontend created SignIn Register and Channel components

--- streamo-client/src/main.jsx
+++ streamo-client/src/main.jsx
@@ -2,18 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from './Components/Home.jsx'
 import { Provider } from 'react-redux'
 import appStore from "./utils/appStore";
+import SignIn from './Components/SignIn.jsx'
+import Channel from './Components/Channel.jsx'
+import Register from './Components/Register.jsx'
 const router = createBrowserRouter([
   {
-  path: "/",
-    element: <App/>,
-    children:[
+    path: "/",
+    element: <App />,
+    children: [
       {
-        path:"/",
-        element: <Home/>
+        path: "/",
+        element: <Home />,
+      },
+      { path: "/signin", element: <SignIn/> },
+      { path: "/channel", element: <Channel/> },
+      {
+        path: "/register",
+        element: <Register />
       }
     ]
   }
@@ -22,7 +31,7 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={appStore}>
-    <RouterProvider router = {router}/>
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 )
