34bd1180e11f63eb0ac1362440657eaf7dac8130
William <145863364+WilliamMark1963@users.noreply.github.com>
2026-05-14 16:07:41 +0530

    At backend a simple get fucntion for welcoming is created

--- streamoBE/src/app.js
+++ streamoBE/src/app.js
@@ -2,5 +2,11 @@ import express from 'express';
 
 const app = new express()
 
+app.use(express.json())
+
+//A simple Grt
+app.get("/streamo",(req,res)=>{
+res.send({message:"Welcome To Streamo Platform"})
+})
 
 export default app;
