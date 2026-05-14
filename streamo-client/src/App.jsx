e03ca6a624e3b6ebf28b3c435eabefa2be1fdad0
William <145863364+WilliamMark1963@users.noreply.github.com>
2026-05-14 16:21:15 +0530

    At frontend Installed react-redux, react-router, tailwindcss, motion, lucide-react

--- streamo-client/src/App.jsx
+++ streamo-client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
+    <div className='flex justify-center items-center text-4xl font-bold bg-neutral-800 h-screen text-white'>
         Streamo App
     </div>
   )
