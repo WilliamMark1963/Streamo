8085594e5dc82a3d6cb61755e3c79f1721bc1633
William <145863364+WilliamMark1963@users.noreply.github.com>
2026-05-14 18:31:34 +0530

    At Backend Added verifyToken Middleware

---
+++ streamoBE/src/Middleware/verifyToken.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import { User } from '../models/User.js';
+
+export const verifyToken = async (req, res, next) => {
+  let token;
+
+  // 1. Check if token exists in Headers (Authorization: Bearer <token>)
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get user from the token (excluding password) and attach to req object
+      req.user = await User.findById(decoded.id).select('-password');
+
+      next(); // Move to the next function (the controller)
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: 'Not authorized, no token' });
+  }
+};
