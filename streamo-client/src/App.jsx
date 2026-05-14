9073e5f7a844182c42a02f1b3f079c575c5a119b
William <145863364+WilliamMark1963@users.noreply.github.com>
2026-05-14 17:12:23 +0530

    At Frontend Added Header Component

--- streamo-client/src/App.jsx
+++ streamo-client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from './Components/Header'
 
 function App() {
   return (
 <div>
-  
+  <Header/>
   <Outlet/>
 </div>
   )
